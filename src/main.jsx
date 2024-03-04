4e24e43526a43ca3793dfa9417e38dae850d560a
webhazrat <mdhazrat445891@gmail.com>
2024-03-04 22:39:28 +0600

    Project 5 Facehook init push

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root, {
-  loader as rootLoader,
-  action as rootAction,
-} from "./routes/Root";
-import Error from "./Error";
-import Contact, { loader as contactLoader } from "./routes/Contact";
-import Edit, { action as editAction } from "./routes/Edit";
-import { action as deleteAction } from "./routes/destroy";
-import Index from "./routes/Index";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <Error />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        index: true,
-        element: <Index />,
-      },
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <Edit />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: deleteAction,
-        errorElement: <div>Oops! There was an error.</div>,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
